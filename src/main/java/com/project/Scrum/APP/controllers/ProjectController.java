7df2049fac3b75b8abc10e33be620f16eaf2401f
jmat732 <jmatisamuel@gmail.com>
2024-09-11 20:34:55 +0200

    comit

--- src/main/java/com/project/Scrum/APP/controllers/ProjectController.java
+++ src/main/java/com/project/Scrum/APP/controllers/ProjectController.java
@@ -1,20 +1,21 @@
 package com.project.Scrum.APP.controllers;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.project.Scrum.APP.models.Project;
 import com.project.Scrum.APP.services.ProjectService;
-import org.springframework.web.bind.annotation.*;
-
 @RestController
 @RequestMapping("/api/projects")
 @CrossOrigin(origins = "*")
 public class ProjectController {
-
     ProjectService projectService;
 
     public ProjectController(ProjectService projectService) {
         this.projectService = projectService;
     }
-
     @PostMapping
     public Project createProject(@RequestBody Project project){
         return projectService.createProject(project);
