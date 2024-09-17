6bfa1f86d07262fccfca52e031c96dd061e31617
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2024-09-17 14:29:34 +0200

    update endpoints AuthController

--- src/main/java/com/project/Scrum/APP/controllers/AuthController.java
+++ src/main/java/com/project/Scrum/APP/controllers/AuthController.java
@@ -17,12 +17,12 @@ public class AuthController {
 
     private final AuthService authService;
 
-    @PostMapping(value = "signin")
+    @PostMapping(value = "login")
     public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request){
         return ResponseEntity.ok(authService.login(request));
     }
 
-    @PostMapping(value = "signup")
+    @PostMapping(value = "register")
     public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request){
         return ResponseEntity.ok(authService.register(request));
     }
