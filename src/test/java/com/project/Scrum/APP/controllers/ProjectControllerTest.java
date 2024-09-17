acb3e3bee1dbf291b4498fa74b04f43f78038d9b
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2024-09-17 11:01:14 +0200

    fix errors test Controllers

--- src/test/java/com/project/Scrum/APP/controllers/ProjectControllerTest.java
+++ src/test/java/com/project/Scrum/APP/controllers/ProjectControllerTest.java
@@ -9,12 +9,14 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.ArrayList;
 import java.util.List;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
