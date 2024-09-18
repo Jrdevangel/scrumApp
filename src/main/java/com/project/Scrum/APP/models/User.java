088030f1e349490108fbb64295076751dc55a2ff
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2024-09-18 12:48:55 +0200

    added getters and setters

--- src/main/java/com/project/Scrum/APP/models/User.java
+++ src/main/java/com/project/Scrum/APP/models/User.java
@@ -32,6 +32,14 @@ public class User implements UserDetails {
     @Column
     private ERole role;
 
+    public Set<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(Set<Project> projects) {
+        this.projects = projects;
+    }
+
     @ManyToMany(
             cascade = CascadeType.ALL,
             fetch = FetchType.LAZY)
@@ -57,6 +65,47 @@ public class User implements UserDetails {
         private int id;
         private String username;
         private String password;
+
+        public ERole getRole() {
+            return role;
+        }
+
+        public void setRole(ERole role) {
+            this.role = role;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
         private String email;
         private ERole role;
 
